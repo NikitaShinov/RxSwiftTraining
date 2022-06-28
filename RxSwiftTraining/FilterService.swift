cb446c5e31f774afba4a69c7adb3d52134398683
Nikita Shinov <shinov_96@mail.ru>
2022-06-28 15:19:17 +0300

    filter func applied

---
+++ RxSwiftTraining/FilterService.swift
@@ -0,0 +1,34 @@
+//
+//  FilterService.swift
+//  RxSwiftTraining
+//
+//  Created by max on 28.06.2022.
+//
+
+import UIKit
+import CoreImage
+
+class FilterService {
+    
+    private var context: CIContext
+    
+    init() {
+        self.context = CIContext()
+    }
+    
+    func applyFilter(to inputImage: UIImage, completion: @escaping((UIImage) -> Void)) {
+        
+        guard let filter = CIFilter(name: "CICMYKHalftone") else { return }
+        filter.setValue(5.0, forKey: kCIInputWidthKey)
+        
+        guard let sourceImage = CIImage(image: inputImage) else { return }
+        
+        filter.setValue(sourceImage, forKey: kCIInputImageKey)
+        if let cgImage = self.context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent) {
+            
+            let processedImage = UIImage(cgImage: cgImage, scale: inputImage.scale, orientation: inputImage.imageOrientation)
+            completion(processedImage)
+        }
+        
+    }
+}
--- RxSwiftTraining/ViewController.swift
+++ RxSwiftTraining/ViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var applyFilterButton: UIButton!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +22,38 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navVC = segue.destination as? UINavigationController,
+              let photosCVC = navVC.viewControllers.first as? PhotosCollectionViewController else {
+                  fatalError()
+              }
+        
+        photosCVC.selectedPhoto.subscribe { [weak self] photo in
+            
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
+            
+        }.disposed(by: disposeBag)
+    }
+    
+    @IBAction func applyFilterButtonPressed() {
+        
+        guard let sourceImge = photoImageView.image else { return }
+        
+        FilterService().applyFilter(to: sourceImge) { filteredImage in
+            DispatchQueue.main.async {
+                self.photoImageView.image = filteredImage
+            }
+        }
+    }
+    
+    private func updateUI(with image: UIImage) {
+        self.photoImageView.image = image
+        self.applyFilterButton.isHidden = false
+    }
 
 }
 
